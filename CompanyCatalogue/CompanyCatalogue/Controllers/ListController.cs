b77f6b8cec209143635ce8ec2719536a7bf4f39b
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-02-06 16:57:15 +0200

    Add list and tree controllers

---
+++ CompanyCatalogue/CompanyCatalogue/Controllers/ListController.cs
@@ -0,0 +1,22 @@
+﻿using CompanyCatalogue.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyCatalogue.Controllers
+{
+    public class ListController : Controller
+    {
+
+        private readonly CatalogueDbContext _dbContext;
+        public ListController(CatalogueDbContext catalogueDbContext)
+        {
+            _dbContext = catalogueDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var res = _dbContext.Employees.ToList();
+
+            return View(res);
+        }
+    }
+}
